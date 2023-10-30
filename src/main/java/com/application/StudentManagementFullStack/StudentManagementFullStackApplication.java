192f0ba7f264af3e91841dd93b2648fcbfc0c70f
adrianpopescupx <adrianpopescupx@gmail.com>
2023-10-30 13:13:38 +0200

    Initialized project

---
+++ src/main/java/com/application/StudentManagementFullStack/StudentManagementFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.application.StudentManagementFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/StudentManagementFullStack/StudentManagementFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.StudentManagementFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
